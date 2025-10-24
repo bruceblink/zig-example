b0fc2c4c37f5b3e65e0c030c13df27773d0b8021
likanug <likanug.g@qq.com>
2025-10-24 12:28:12 +0800

    refactor: 优化字符串格式化输出，增强可读性

--- src/string.zig
+++ src/string.zig
@@ -1,17 +1,23 @@
 const std = @import("std");
-const debug = @import("std").debug;
-const print = debug.print;
+const print = std.debug.print;
+const expect = std.testing.expect;
+const eql = std.mem.eql;
 
 test "test string" {
 
     // 格式化时，可以使用 u 输出对应的字符
     const me_zh = '我';
-    print("{0u} = {0x}\n", .{me_zh}); // 我 = 6211
-
+    print("\n{0u} = {0x}\n", .{me_zh}); // 我 = 6211
+    try expect(me_zh == 0x6211);
     // 如果是 ASCII 字符，还可以使用 c 进行格式化
     const me_en = 'I';
-    print("{0u} = {0c} = {0x}\n", .{me_en}); // I = I = 49
+    try expect(me_en == 0x49);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    const formatted = try std.fmt.allocPrint(allocator, "{0u} = {0c} = {0x}", .{me_en});
+    defer allocator.free(formatted);
 
+    try expect(eql(u8, formatted, "I = I = 49")); // ✅ 断言字符串完全匹配
     // 下面的写法会报错，因为这些 emoji 虽然看上去只有一个字，但其实需要由多个码位组合而成
     // const hand = '🖐🏽';
     // const flag = '🇨🇳';
